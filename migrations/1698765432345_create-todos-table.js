db5270622ab8d295a60eb34040a0ec051c590001
Abhishek Veeramalla <abhishek.veeramalla@gmail.com>
2025-02-27 18:44:54 +0530

    Update 1698765432345_create-todos-table.js

--- migrations/1698765432345_create-todos-table.js
+++ migrations/1698765432345_create-todos-table.js
@@ -3,11 +3,6 @@ exports.up = (pgm) => {
     id: 'id',
     title: { type: 'varchar(255)', notNull: true },
     completed: { type: 'boolean', notNull: true, default: false },
-    created_at: {
-      type: 'timestamp',
-      notNull: true,
-      default: pgm.func('current_timestamp'),
-    },
   });
 };
 
