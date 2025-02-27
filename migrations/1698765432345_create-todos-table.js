bbe4d65adc4f809685ab9059efcbd82b636cf578
Abhishek Veeramalla <abhishek.veeramalla@gmail.com>
2025-02-27 17:57:02 +0530

    chore: update db schema

--- migrations/1698765432345_create-todos-table.js
+++ migrations/1698765432345_create-todos-table.js
@@ -2,7 +2,7 @@ exports.up = (pgm) => {
   pgm.createTable('todos', {
     id: 'id',
     title: { type: 'varchar(255)', notNull: true },
-    // completed: { type: 'boolean', notNull: true, default: false },
+    completed: { type: 'boolean', notNull: true, default: false },
     created_at: {
       type: 'timestamp',
       notNull: true,
